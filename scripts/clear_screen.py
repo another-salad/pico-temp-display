ff78507571dde45054e7feffdaf76a68a38726a2
anothersalad <48966874+another-salad@users.noreply.github.com>
2024-09-05 01:10:35 +0100

    better or something

--- scripts/clear_screen.py
+++ scripts/clear_screen.py
@@ -5,4 +5,4 @@ Runtime script (can be triggered by cron, etc)
 if __name__ == "__main__":
     from common import Args, req
     arg, _ = Args().parse_known_args()
-    req(f"http://{arg.pico}/set-colour-cycle")
+    req(f"http://{arg.pico}/clear")
